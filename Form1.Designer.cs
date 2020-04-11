1b6bd26c14199556518635054ed44ca223037b78
Learnuel-crypto <63509139+Learnuel-crypto@users.noreply.github.com>
2020-04-11 19:10:00 +0100

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace BackFile
+    {
+    partial class BackupForm
+        {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+            {
+            if (disposing && (components != null))
+                {
+                components.Dispose();
+                }
+            base.Dispose(disposing);
+            }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+            {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BackupForm));
+            this.pictureWait = new System.Windows.Forms.PictureBox();
+            this.lblWait = new System.Windows.Forms.Label();
+            this.btnBackup = new System.Windows.Forms.Button();
+            this.btnOutfutFolder = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtFolderName = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnClose = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureWait)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureWait
+            // 
+            this.pictureWait.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureWait.Location = new System.Drawing.Point(4, 1);
+            this.pictureWait.Name = "pictureWait";
+            this.pictureWait.Size = new System.Drawing.Size(436, 145);
+            this.pictureWait.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureWait.TabIndex = 119;
+            this.pictureWait.TabStop = false;
+            this.pictureWait.Visible = false;
+            // 
+            // lblWait
+            // 
+            this.lblWait.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblWait.AutoSize = true;
+            this.lblWait.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWait.Location = new System.Drawing.Point(166, 159);
+            this.lblWait.Name = "lblWait";
+            this.lblWait.Size = new System.Drawing.Size(90, 21);
+            this.lblWait.TabIndex = 120;
+            this.lblWait.Text = "Please wait";
+            this.lblWait.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.lblWait.Visible = false;
+            // 
+            // btnBackup
+            // 
+            this.btnBackup.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBackup.BackColor = System.Drawing.Color.White;
+            this.btnBackup.FlatAppearance.BorderSize = 0;
+            this.btnBackup.FlatAppearance.CheckedBackColor = System.Drawing.Color.Black;
+            this.btnBackup.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightGray;
+            this.btnBackup.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBackup.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBackup.ForeColor = System.Drawing.Color.Black;
+            this.btnBackup.Location = new System.Drawing.Point(214, 78);
+            this.btnBackup.Name = "btnBackup";
+            this.btnBackup.Size = new System.Drawing.Size(77, 32);
+            this.btnBackup.TabIndex = 118;
+            this.btnBackup.Text = "Backup";
+            this.btnBackup.UseVisualStyleBackColor = false;
+            this.btnBackup.Click += new System.EventHandler(this.btnBackup_Click);
+            // 
+            // btnOutfutFolder
+            // 
+            this.btnOutfutFolder.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOutfutFolder.FlatAppearance.BorderSize = 0;
+            this.btnOutfutFolder.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOutfutFolder.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOutfutFolder.Location = new System.Drawing.Point(380, 47);
+            this.btnOutfutFolder.Name = "btnOutfutFolder";
+            this.btnOutfutFolder.Size = new System.Drawing.Size(46, 29);
+            this.btnOutfutFolder.TabIndex = 115;
+            this.btnOutfutFolder.Text = ". . .";
+            this.btnOutfutFolder.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnOutfutFolder.UseVisualStyleBackColor = true;
+            this.btnOutfutFolder.Click += new System.EventHandler(this.btnOutfutFolder_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(158, 17);
+            this.label2.TabIndex = 117;
+            this.label2.Text = "Select Destination Folder";
+            // 
+            // txtFolderName
+            // 
+            this.txtFolderName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFolderName.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFolderName.Location = new System.Drawing.Point(12, 47);
+            this.txtFolderName.Name = "txtFolderName";
+            this.txtFolderName.ReadOnly = true;
+            this.txtFolderName.Size = new System.Drawing.Size(362, 25);
+            this.txtFolderName.TabIndex = 116;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.BackColor = System.Drawing.Color.White;
+            this.btnClose.FlatAppearance.BorderSize = 0;
+            this.btnClose.FlatAppearance.CheckedBackColor = System.Drawing.Color.Black;
+            this.btnClose.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightGray;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.ForeColor = System.Drawing.Color.Black;
+            this.btnClose.Location = new System.Drawing.Point(297, 78);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(77, 32);
+            this.btnClose.TabIndex = 121;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // BackupForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(441, 199);
+            this.Controls.Add(this.pictureWait);
+            this.Controls.Add(this.lblWait);
+            this.Controls.Add(this.btnBackup);
+            this.Controls.Add(this.btnOutfutFolder);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtFolderName);
+            this.Controls.Add(this.btnClose);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "BackupForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Backup";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.BackupForm_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureWait)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+            }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureWait;
+        private System.Windows.Forms.Label lblWait;
+        private System.Windows.Forms.Button btnBackup;
+        private System.Windows.Forms.Button btnOutfutFolder;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtFolderName;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnClose;
+        }
+    }
+
---
+++ Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using FileEncrypt;
+using System.Diagnostics;
+
+namespace BackFile
+    {
+    public partial class BackupForm : Form
+        {
+        DataEncryptor dataCryptor = new DataEncryptor();
+        DataEncryptor dataCryptor2 = new DataEncryptor();
+        FileInfo info;
+        FileInfo info2;
+        public BackupForm()
+            {
+            InitializeComponent();
+            }
+
+        private void btnOutfutFolder_Click(object sender , EventArgs e)
+            {
+            OpenFileDialog folderBrowser = new OpenFileDialog();
+            // Set validate names and check file exists to false otherwise windows will
+            // not let you select "Folder Selection."
+            folderBrowser.RestoreDirectory = true;
+            folderBrowser.ValidateNames = false;
+            folderBrowser.CheckFileExists = false;
+            folderBrowser.CheckPathExists = true;
+            // Always default to Folder Selection.
+            folderBrowser.FileName = "Folder Selection.";
+            if (folderBrowser.ShowDialog() == DialogResult.OK)
+                {
+                dataCryptor.outPutFilePath(Path.GetDirectoryName(folderBrowser.FileName));
+                string folderPath = Path.GetDirectoryName(folderBrowser.FileName);
+                txtFolderName.Text = folderPath;
+                // ...
+                }
+            }
+
+        private void btnBackup_Click(object sender , EventArgs e)
+            {
+                try
+                {
+                    if(string.IsNullOrEmpty(txtFolderName.Text)){
+                    throw new Exception("Select folder to backup file");
+                    }
+                    timer1.Enabled = true;
+                    timer1.Start();
+
+                    FileInfo infor = new FileInfo(Application.StartupPath + "/CASHDESKDB.mdf");
+                    info = new FileInfo(infor.FullName);
+                    dataCryptor.inPutFilePath(infor.FullName);
+                    dataCryptor.FileType(info.Extension);
+                    dataCryptor.FileType(info.Name.Substring(0, info.Name.Length - info.Extension.Length), info.Extension);
+                    dataCryptor.isDeletePlainFile(false);
+                    dataCryptor.EncryptFile();
+
+                    FileInfo infor2 = new FileInfo(Application.StartupPath + "/CASHDESKDB_log.ldf");
+                    info2 = new FileInfo(infor2.FullName);
+                    dataCryptor2.inPutFilePath(infor2.FullName);
+                    dataCryptor2.FileType(info2.Extension);
+                    dataCryptor2.FileType(info2.Name.Substring(0, info2.Name.Length - info2.Extension.Length), info2.Extension);
+                    dataCryptor2.isDeletePlainFile(false);
+                    dataCryptor2.EncryptFile();
+                    MessageBox.Show("Backup Sucessfull", "Cashdesk", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    timer1.Stop();
+                    pictureWait.Visible = false;
+                    lblWait.Visible = false;
+
+                }
+                catch (System.UnauthorizedAccessException)
+                {
+                    MessageBox.Show("Access to File is denied for backup", "Cashdesk", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+                catch (Exception ex)
+                {
+                    pictureWait.Visible = false;
+                    lblWait.Visible = false;
+                    timer1.Stop();
+                    MessageBox.Show( ex.Message, "Cashdesk", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+
+        private void btnClose_Click(object sender , EventArgs e)
+            {
+            try
+                {
+                ProcessStartInfo info = new ProcessStartInfo("WindowsFormsApplication1.exe");
+                info.UseShellExecute = true;
+                info.Verb = "runas";
+                Process.Start(info);
+                Application.ExitThread();
+                }
+            catch (Exception)
+                {
+                MessageBox.Show("Specified File was not found" , "Cash Desk" , MessageBoxButtons.OK , MessageBoxIcon.Information);
+                }
+            }
+        
+
+        private void BackupForm_FormClosed(object sender , FormClosedEventArgs e)
+            {
+            try
+                {
+                ProcessStartInfo info = new ProcessStartInfo("WindowsFormsApplication1.exe");
+                info.UseShellExecute = true;
+                info.Verb = "runas";
+                Process.Start(info);
+                Application.ExitThread();
+                }
+            catch (Exception)
+                {
+                MessageBox.Show("Specified File was not found" , "Cash Desk" , MessageBoxButtons.OK , MessageBoxIcon.Information);
+                }
+            }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pictureWait.Visible = true;
+            lblWait.Visible = true;
+        }
+        
+        }
+    }
